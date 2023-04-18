14af9ec37aa455273cd75f046909509596292fc7
fandi-adhitya <fandiadhitya96@gmail.com>
2023-04-18 14:46:29 +0700

    remove package

--- lib/screens/screen.dart
+++ lib/screens/screen.dart
@@ -8,11 +8,9 @@ import 'package:algostudiocodingtest/widgets/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_share_me/flutter_share_me.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:flutter_share_me/flutter_share_me.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../helpers/generate_random.dart';
